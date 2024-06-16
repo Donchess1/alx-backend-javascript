2485931b8eb3c7fa962a93f6f6a852db3a2dcfbd
Donchess1 <Stepabod@yahoo.com>
2024-06-16 17:40:47 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+  const username = process.stdin.read();
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  if (username) {
+    process.stdout.write(`Your name is: ${username}`);
   }
 });
 
